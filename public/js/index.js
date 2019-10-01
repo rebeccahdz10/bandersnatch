a985c989d56750a115810f49221f50dc5b4c3923
Spaghetti Jones <connor.breault@comcast.net>
2019-09-30 23:19:59 -0700

    home page added and linked to login button, basic login css

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,25 @@
-<<<<<<< HEAD
-$(".this").on("click", function () {
-    alert("gang gang")
-})
-=======
 var loginPeeps = [
     {
-    username: `marissa`,
-    password: `mc1234`
-},
-{
-    username: `rebecca`,
-    password: `rh1234`
-},
-{
-    username: `connor`,
-    password: `cb1234`
-}
+        username: `marissa`,
+        password: `mc1234`
+    },
+    {
+        username: `rebecca`,
+        password: `rh1234`
+    },
+    {
+        username: `connor`,
+        password: `cb1234`
+    }
 ]
 function getInfo() {
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
-    for(i = 0; i < loginPeeps.length; i++) {
-        if(username == loginPeeps[i].username && password == loginPeeps[i].password) {
+    for (i = 0; i < loginPeeps.length; i++) {
+        if (username == loginPeeps[i].username && password == loginPeeps[i].password) {
             console.log(username + " Thanks for logging in!")
             return
         }
     }
     console.log("sorry bro you not one of us!")
 }
->>>>>>> fbf2197261b3a543a18d01ca1c85e8d200eef71f
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,15 @@ module.exports = function (app) {
         });
     });
 
+    // Load home page
+    app.get("/home", function (req, res) {
+        db.Example.findAll({}).then(function (dbExamples) {
+            res.render("home", {
+                examples: dbExamples
+            });
+        });
+    });
+
     // Load example page and pass in an example by id
     app.get("/questions/:id", function (req, res) {
         db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
