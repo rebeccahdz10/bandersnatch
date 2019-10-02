5474af686c5d476d7fa04eecd7947dc0c9abe07d
Spaghetti Jones <connor.breault@comcast.net>
2019-10-01 19:21:41 -0700

    stash2

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,119 +1,195 @@
-INSERT INTO mainDialogue (question, option1, option2) 
-VALUES ("What do you want for breakfast?", "Acai bowl", "Leftover pizza");
+INSERT INTO mainDialogue
+    (question, option1, option2)
+VALUES
+    ("What do you want for breakfast?", "Acai bowl", "Leftover pizza");
 
-INSERT INTO mainDialogue (question, option1, option2) 
-VALUES ("What do you want to do today?", "Visit a Friend", "Run an Errand");
-
-
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("How you living?", "Boolin", "Lost in the sauce");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("What's new?");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("What's wrong?");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("What's your drink?", "Beer", "Shot");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("What type of beer?", "PBR", "IPA");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("Do you take them?", "Yes", "No");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("Do you eat them right away?", "Yes", "No");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("Do you want to go home?", "Yes", "No");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("What type of shot?", "Vodka", "Tequila");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("Do you punch him?", "Yes", "No");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("Do you want to finish him?", "Yes", "No");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("Do you want to dance?", "Yes", "No");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("Do you want to head home?", "Yes", "No");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("What do you do?", "Run", "Wait");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("Do you want to dance?", "Yes", "No");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("Do you take it?", "Yes", "No");
-
-INSERT INTO adventureOne (question, option1, option2)
-VALUES ("Take it?", "Yes", "No");
+INSERT INTO mainDialogue
+    (question, option1, option2)
+VALUES
+    ("What do you want to do today?", "Visit a Friend", "Run an Errand");
 
 
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What errand do you want to run?");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("How you living?", "Boolin", "Lost in the sauce");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("How do you fix it?", "Yourself", "Call a tow truck");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("What's new?");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What do you say?", "Let's go", "Nah, I'm busy");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("What's wrong?");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What do you do?", "Go home", "Go to the bank");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("What's your drink?", "Beer", "Shot");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("Do you take them?", "Yes", "No");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("What type of beer?", "PBR", "IPA");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What do you do?", "Act cool", "Run!");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("Do you take them?", "Yes", "No");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What do you do?", "Rob the place yourself", "Point it at the robber");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("Do you eat them right away?", "Yes", "No");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What do you do?", "Grab a hostage and run!", "Surrender");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("Do you want to go home?", "Yes", "No");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What do you do?", "Put the gun down", "Shoot the hostage");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("What type of shot?", "Vodka", "Tequila");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What do you do?", "Shoot him", "Chase him");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("Do you punch him?", "Yes", "No");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("How do you celebrate?", "Go home", "Go to the bar");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("Do you want to finish him?", "Yes", "No");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("Do you take it?", "Nah, I want a beer", "Hell yeah it's free!");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("Do you want to dance?", "Yes", "No");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("Take it?", "No, it's time for food", "Yes!");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("Do you want to head home?", "Yes", "No");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("Do you take it?", "No thanks", "Yeah, what's the worst that can happen?");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("What do you do?", "Run", "Wait");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("Do you call a taxi or an uber?", "Taxi", "Uber");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("Do you want to dance?", "Yes", "No");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What do you do?", "Go take a look", "Disregard it");
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("Do you take it?", "Yes", "No");
 
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What do you do?", "Punch him", "Ask for forgiveness");
-
-INSERT INTO adventureTwo (question, option1, option2)
-VALUES ("What do you do?", "Fight him off", "Run!");
-
+INSERT INTO adventureOne
+    (question, option1, option2)
+VALUES
+    ("Take it?", "Yes", "No");
 
 
-INSERT INTO endings (adventure, story)
-VALUES ("One", "")
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What errand do you want to run?");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("How do you fix it?", "Yourself", "Call a tow truck");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What do you say?", "Let's go", "Nah, I'm busy");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What do you do?", "Go home", "Go to the bank");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("Do you take them?", "Yes", "No");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What do you do?", "Act cool", "Run!");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What do you do?", "Rob the place yourself", "Point it at the robber");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What do you do?", "Grab a hostage and run!", "Surrender");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What do you do?", "Put the gun down", "Shoot the hostage");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What do you do?", "Shoot him", "Chase him");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("How do you celebrate?", "Go home", "Go to the bar");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("Do you take it?", "Nah, I want a beer", "Hell yeah it's free!");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("Take it?", "No, it's time for food", "Yes!");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("Do you take it?", "No thanks", "Yeah, what's the worst that can happen?");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("Do you call a taxi or an uber?", "Taxi", "Uber");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What do you do?", "Go take a look", "Disregard it");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What do you do?", "Punch him", "Ask for forgiveness");
+
+INSERT INTO adventureTwo
+    (question, option1, option2)
+VALUES
+    ("What do you do?", "Fight him off", "Run!");
+
+
+
+INSERT INTO endings
+    (adventure, story)
+VALUES
+    ("One", "fart")
