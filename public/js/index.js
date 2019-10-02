8509b55f8afd3a20d1a18815cb19bd9e95905e3a
Rissaree <51396335+mcarrillo916@users.noreply.github.com>
2019-10-01 19:10:45 -0700

    blah

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,22 @@
 var loginPeeps = [
     {
+    email: `blah@blah.com`,
     username: `marissa`,
     password: `mc1234`
 },
 {
+    email: `yourmom@blah.com`,
     username: `rebecca`,
     password: `rh1234`
 },
 {
+    email: `music@blah.com`,
     username: `connor`,
     password: `cb1234`
 }
 ]
 function getInfo() {
+    var email = document.getElementById("email").value
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
     for(i = 0; i < loginPeeps.length; i++) {
